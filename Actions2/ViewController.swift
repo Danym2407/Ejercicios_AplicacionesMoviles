30b4581966398652010b35e3b02147af9fe35c19
Danym2407 <danym2407@gmail.com>
2025-02-04 11:07:00 -0600

    Realizamos una impresion de pantalla a base de un boton, cambiando el mensaje de Holaaa por Adiooooooos

--- Actions2/ViewController.swift
+++ Actions2/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Greetme(_ sender: Any) {
+        Label1.text = "Adiooooos Bebe";
+    }
+    
 }
 

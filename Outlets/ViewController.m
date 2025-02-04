405a9c763f8ef4243b2d36ae1b0ca17a0509fb0c
Danym2407 <danym2407@gmail.com>
2025-02-04 10:06:30 -0600

    Durante este peque;o proyecto evaluaremos como visualizar texto en pantalla, a traves de LABELS. En este primer caso agregamos 2 labels, para en el viewcontrol manejar lo que vemos a la salida

--- Outlets/ViewController.m
+++ Outlets/ViewController.m
@@ -8,6 +8,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *LabelOne;
+@property (weak, nonatomic) IBOutlet UILabel *LabelTwo;
 
 @end
 
@@ -16,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //Escribimos lo que queremos que aparezca en el output
+    [_LabelOne setText:@"Hola soy la linea 1"];
+    [_LabelTwo setText:@"Soy linea 2"];
 }
 
 

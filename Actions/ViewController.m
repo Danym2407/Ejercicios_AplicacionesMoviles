b83b062afafcaf1d2bbcb69b133fc724d3ae5299
Danym2407 <danym2407@gmail.com>
2025-02-04 10:10:50 -0600

    Initial Commit

---
+++ Actions/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Actions
+//
+//  Created by Daniela Mendez on 04/02/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

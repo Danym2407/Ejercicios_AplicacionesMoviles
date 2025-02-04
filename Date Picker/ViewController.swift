4db299d1949a3e3881f670d50ea19f450b391902
Danym2407 <danym2407@gmail.com>
2025-02-04 11:36:48 -0600

    Realizamos un DatePicker con el que podremos capturar la fecha que el usuario eligio

--- Date Picker/ViewController.swift
+++ Date Picker/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func readDate(_ sender: Any) {
+        
+        //Obtenemos la fecha seleccionada
+        let selectedDate = datePicker.date
+        
+        //Creamos un formato para la fecha
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        //Convertimos la fecha en un string
+        let dateString = dateFormatter.string(from: selectedDate)
+        
+        dateLabel.text = dateString
+        
+    }
+    
 }
 
